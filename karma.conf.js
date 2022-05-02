8d79e3e0079038a77cfe228fa1f2abf803093d1f
Kyle Wilichowski <kyle.wilichowski@jbhunt.com>
2022-05-02 15:53:41 -0500

    updates for gh-pages

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/generation-schedule-2.0'),
+      dir: require('path').join(__dirname, './coverage/generation-schedule-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'generation-schedule-2.0'`, () => {
+  it(`should have as title 'generation-schedule-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('generation-schedule-2.0');
+    expect(app.title).toEqual('generation-schedule-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('generation-schedule-2.0 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('generation-schedule-ui app is running!');
   });
 });
